1d00a9eea04391c703bab8c107eb3adcba816b31
Krishnakumar <krishnakumars0101@gmail.com>
2025-02-01 01:09:02 +0530

    bug fixed

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const cors = require('cors');
-require("dotenv").config();
-const MONGO_URL = process.env.MONGO_URL;
+require('dotenv').config();
 
-const Transaction =require('./models/Transaction.js');
+
+const Transaction =require('./models/Transaction');
 const { default: mongoose } = require('mongoose');
 const app = express();
 const PORT = 4000;
@@ -16,15 +16,11 @@ app.get('/api/test', (req, res) => {
     res.json({ body: 'test ok2' });
 });
 
-app.post('/api/transaction',(req, res) => {
+app.post('/api/transaction',async (req, res) => {
+    await mongoose.connect('process.env.MONGO_URL')
     const { name,description,datetime } = req.body;
-    console.log(MONGO_URL)
-// mongoose.connect('')
-    
-    res.json({
-        message: 'Transaction received successfully!',
-        transaction: { name,description,datetime }
-    });
+    const transaction=await Transaction.create({name,description,datetime})
+    res.json(transaction);
 });
 
 app.listen(PORT, () => {
